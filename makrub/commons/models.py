7814e64c02035e6d7a847f20ce51f40652ffcf30
Pob <590650@gmail.com>
2018-02-18 01:13:23 +0700

    use verbose_name

--- makrub/commons/models.py
+++ makrub/commons/models.py
@@ -3,7 +3,7 @@ from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 class Room(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,verbose_name='this is your room\'s name')
     description = models.TextField()
     user_id = models.IntegerField()
     account_id = models.IntegerField()
