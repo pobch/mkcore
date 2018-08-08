661e787ef74c361cfad86f6ed0d82d91ee27fda0
Metz Charusasi <metz@studiotwist.co>
2018-08-08 10:00:41 +0700

    remove autoreload on deploy

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ echo "Running database migrations ..."
 python manage.py migrate
 echo "Successfully migrated database"
 
-if [ "$APP_ENV" = "prod" ]; then
-    /usr/local/bin/uwsgi --http :8000 --wsgi-file config/wsgi.py
-else
+if [ "$APP_ENV" = "local" ]; then
     /usr/local/bin/uwsgi --http :8000 --wsgi-file config/wsgi.py --py-autoreload 1
+else
+    /usr/local/bin/uwsgi --http :8000 --wsgi-file config/wsgi.py
 fi
