9b85d0fd21081bd59b6c44709cfa593802b0e4a7
Pob <590650@gmail.com>
2018-08-18 09:28:32 +0700

    add some useful comments

--- makrub/core/admin.py
+++ makrub/core/admin.py
@@ -9,3 +9,6 @@ admin.site.register(UserProfile)
 admin.site.register(Room)
 admin.site.register(RoomAnswer)
 admin.site.register(GuestRoomRelation)
+
+# To integrate my custom User model with django's admin site, see :
+# https://www.fomfus.com/articles/how-to-use-email-as-username-for-django-authentication-removing-the-username
