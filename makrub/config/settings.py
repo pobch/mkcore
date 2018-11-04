728a8b16b2dc7bc3aaea000480aa8e7ca7cf7429
Pob <590650@gmail.com>
2018-11-05 00:07:08 +0700

    add mkcore-app.herokuapp.com to CORS setting

--- makrub/config/settings.py
+++ makrub/config/settings.py
@@ -80,9 +80,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -201,6 +201,7 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
     'dev.makrub.com',
     'dev-app.makrub.com',
+    'mkcore-app.herokuapp.com',
 )
 
 
